1ac6c38fa017f2e0cb71c8e900c2cc45fca9e1ed
Peter Wood <peter@griffin.sh>
2022-10-31 15:11:04 +0000

    Add build info output on run.

--- main.go
+++ main.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/pwood/fdbexplorer/data"
 	"github.com/pwood/fdbexplorer/input"
 	"github.com/pwood/fdbexplorer/output"
 	"os"
+	"runtime/debug"
 )
 
 func main() {
+	header()
+	
 	ch := make(chan data.State)
 	defer close(ch)
 
 	if in := input.Select(ch); in == nil {
-		flag.PrintDefaults()
-		os.Exit(1)
+		usage()
 	} else {
 		go in.Run()
 	}
 
 	if out := output.Select(ch); out == nil {
-		flag.PrintDefaults()
-		os.Exit(1)
+		usage()
 	} else {
 		out.Run()
 	}
+}
+
+func header() {
+	bi, _ := debug.ReadBuildInfo()
+	version := bi.Main.Version
+	sum := bi.Main.Sum
+
+	fmt.Printf("fdbexplorer %s (%s)\n\n", version, sum)
+}
+
+func usage() {
+	flag.PrintDefaults()
+	os.Exit(1)
 }
